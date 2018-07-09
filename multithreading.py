80e4311b4632ce44d56e66a89e5d7935f2224724
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-07-08 23:27:31 -0400

    feat: Add count until limit time

--- multithreading.py
+++ multithreading.py
@@ -1,5 +1,6 @@
 
 import threading
+import time
 
 
 def simple_count(factor_limit=1):
@@ -17,4 +18,33 @@ def simple_count(factor_limit=1):
 N_THREADS = 5
 for n_thread in range(N_THREADS):
     thread = threading.Thread(name="thread_%d" % n_thread, target=simple_count, args=(n_thread,))
+    thread.start()
+
+
+vmax_threads = {}
+
+
+def count_seconds(seconds):
+    """Count until time limit"""
+    global vmax_threads
+    counter = 0
+    start = time.time()
+    limit = start + seconds
+    name = threading.current_thread().getName()
+    while start <= limit:
+        counter += 1
+        start = time.time()
+        print(name, counter)
+
+    vmax_threads[name] = counter
+    if threading.active_count() == 2:
+        print(vmax_threads)
+        print(threading.enumerate())
+
+
+seconds = 2
+for n_thread in range(5):
+    thread = threading.Thread(name='thread%s' % n_thread,
+                              target=contar,
+                              args=(seconds,))
     thread.start()
