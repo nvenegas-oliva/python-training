c5fd18c8425d2a6c4d43a72826ddd232c70dfa0a
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-07-08 23:00:23 -0400

    feat: Add args

--- multithreading.py
+++ multithreading.py
@@ -2,18 +2,19 @@
 import threading
 
 
-def simple_count():
+def simple_count(factor_limit=1):
     counter = 0
-    while counter < 1000000:
+    while counter < 10000 * factor_limit:
         counter += 1
-        print("thread.name=%s\tid=%d\tcounter=%d" % (
+        print("thread.name=%s\tid=%d\tfactor_limit=%d\tcounter=%d" % (
             threading.current_thread().getName(),
             threading.current_thread().ident,
+            factor_limit,
             counter)
         )
 
 
 N_THREADS = 5
 for n_thread in range(N_THREADS):
-    thread = threading.Thread(name="thread_%d" % n_thread, target=simple_count)
+    thread = threading.Thread(name="thread_%d" % n_thread, target=simple_count, args=(n_thread))
     thread.start()
