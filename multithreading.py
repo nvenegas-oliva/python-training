fd1fe5915d5d635d8b662165a233e24a82d072fb
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-07-08 23:09:03 -0400

    fix: Pass an iterable

--- multithreading.py
+++ multithreading.py
@@ -4,7 +4,7 @@ import threading
 
 def simple_count(factor_limit=1):
     counter = 0
-    while counter < 10000 * factor_limit:
+    while counter < 100000 * factor_limit:
         counter += 1
         print("thread.name=%s\tid=%d\tfactor_limit=%d\tcounter=%d" % (
             threading.current_thread().getName(),
@@ -16,5 +16,5 @@ def simple_count(factor_limit=1):
 
 N_THREADS = 5
 for n_thread in range(N_THREADS):
-    thread = threading.Thread(name="thread_%d" % n_thread, target=simple_count, args=(n_thread))
+    thread = threading.Thread(name="thread_%d" % n_thread, target=simple_count, args=(n_thread,))
     thread.start()
