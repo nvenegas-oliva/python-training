eec03d71fbde4f7085a2c9036b2bf0620e6cdfbe
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-07-08 23:47:33 -0400

    feat: Add check and write_file functions

--- daemons.py
+++ daemons.py
@@ -1,3 +1,48 @@
 import os
 import time
 import threading
+
+
+def check(name):
+    '''Check the size of file'''
+    counter = 0
+    size = 0
+    while counter < 100:
+        counter += 1
+        if os.path.exists(name):
+            state = os.stat(name)
+            size = state.st_size
+
+        print(threading.current_thread().getName(),
+              counter,
+              size,
+              'bytes')
+
+        time.sleep(0.1)
+
+
+def write_file(name):
+    counter = 1
+    while counter <= 10:
+        with open(name, 'a') as file:
+            file.write('1')
+            print(threading.current_thread().getName(),
+                  counter)
+            time.sleep(0.3)
+            counter += 1
+
+
+file_name = 'file.txt'
+if os.path.exists(file_name):
+    os.remove(file_name)
+
+thread_1 = threading.Thread(name='check',
+                            target=check,
+                            args=(file_name,),
+                            daemon=True)
+
+thread_2 = threading.Thread(name='write_file',
+                            target=write_file,
+                            args=(file_name,))
+thread_1.start()
+thread_2.start()
