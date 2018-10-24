37ebad35fc78967bbb4dc7ed9c217e5272866f85
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-10-24 15:47:17 -0300

    feat: Don't use the environent variable

--- concurrency/single.py
+++ concurrency/single.py
@@ -1,8 +1,7 @@
 import logging
-import os
 from time import time
 
-from download import setup_download_dir, get_links, download_link
+from concurrency.download import setup_download_dir, get_links, download_link
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,11 +10,13 @@ logger = logging.getLogger(__name__)
 
 def main():
     ts = time()
-    client_id = os.getenv('IMGUR_CLIENT_ID')
+    # client_id = os.getenv('IMGUR_CLIENT_ID')
+    client_id = "1aadb806e0e54b9"
     if not client_id:
         raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
     download_dir = setup_download_dir()
     links = get_links(client_id)
+    len(get_links(client_id))
     for link in links:
         download_link(download_dir, link)
     logging.info('Took %s seconds', time() - ts)
