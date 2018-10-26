1de9a6a63c52be780f05ea9cbb1dff2d003e4ec2
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-10-26 03:29:40 -0300

    feat: Creata file

---
+++ time-series/20181025.py
@@ -0,0 +1,27 @@
+
+import pandas as pd
+
+pd.read_csv('time-series/appl_1980_2014.csv').head()
+
+df = pd.read_csv('time-series/appl_1980_2014.csv', parse_dates=['Date'], index_col='Date')
+df.info()
+
+
+df.resample('BM').count()
+
+df.index.max() - df.index.min()
+(df.index.max() - df.index.min()).days
+
+
+len(df.resample('M').count().index)
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+%matplotlib inline
+sns.set()
+
+
+_ = plt.figure(figsize=(18, 6))
+_ = df['Adj Close'].plot()
